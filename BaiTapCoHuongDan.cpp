76e115965cc517277467a33f7da3187951a5a969
A105PC35 <hellow12343@gmail.com>
2024-07-17 13:43:42 +0700

    completed function read and write students list

--- BaiTapCoHuongDan.cpp
+++ BaiTapCoHuongDan.cpp
@@ -22,3 +22,42 @@ void xuatTTSV(ItemType x)
 {
 	printf("%-15s%-10s%-20s%-10s%5.2lf\n", x.mssv, x.ho, x.hoLot, x.ten, x.diemTB);
 }
+void docDSSV(char fNameIn[], ItemType ds[], int &n)
+{
+	FILE *fi = fopen(fNameIn, "rt");
+	if (fi == NULL)
+	{
+		printf("Loi: mo file de doc du lieu.");
+		getch();
+		return;
+	}
+	fscanf(fi, "%d\n", &n);
+	for (int i = 0; i < n; i++)
+	{
+		SV x;
+		docTTSV(fi, x);
+		ds[i] = x;
+	}
+	fclose(fi);
+}
+void ghiTTSV(FILE *fo, ItemType x)
+{
+	fprintf(fo, "%-15s%-10s%-20s%-10s%5.2lf\n", x.mssv, x.ho, x.hoLot, x.ten, x.diemTB);
+}
+void ghiDSSV(char fNameOut[], ItemType ds[], int n)
+{
+	FILE *fo = fopen(fNameOut, "wt");
+	if (fo == NULL)
+	{
+		printf("Loi: mo file de ghi du lieu.");
+		getch();
+		return;
+	}
+	fprintf(fo, "%-5s%-15s%-40s%-10s\n", "STT", "MSSV", "HO VA TEN SINH VIEN", "DIEM TB");
+	for (int i = 0; i < n; i++)
+	{
+		fprintf(fo, "%-5d", i + 1);
+		ghiTTSV(fo, ds[i]);
+	}
+	fclose(fo);
+}
